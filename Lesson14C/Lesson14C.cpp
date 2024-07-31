c3f69ed1b972d12d1232ddd5ffe1929fb8b93d0b
Evgeniy Armand <igestam@gmail.com>
2024-07-31 16:09:49 +0300

    Lesson14

---
+++ Lesson14C/Lesson14C.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+int main() {
+    // Устанавливаем кодировку консоли на UTF-8
+    SetConsoleOutputCP(CP_UTF8);
+
+    std::string myString = "Пример строки";
+
+    std::cout << "Строковая переменная: " << myString << std::endl;
+    std::cout << "Длина строки: " << myString.length() << std::endl;
+    std::cout << "Первый символ: " << myString.front() << std::endl;
+    std::cout << "Последний символ: " << myString.back() << std::endl;
+
+    return 0;
+}
